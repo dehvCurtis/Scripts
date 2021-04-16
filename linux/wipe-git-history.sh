a037d04b4baa38e51904295bd16df5edd6ceee85
Dehvon Curtis <dcurtis@berkadia.com>
2021-04-16 09:00:23 -0600

    added script

--- linux/wipe-git-history.sh
+++ linux/wipe-git-history.sh
@@ -16,3 +16,13 @@ git add .
 git commit -m 'Initial Commit'
 git remote add origin $GIT_REPO
 git push -u --force origin master
+
+
+# better way to do this
+# git checkout --orphan newBranch
+# git add -A  # Add all files and commit them
+# git commit
+# git branch -D master  # Deletes the master branch
+# git branch -m master  # Rename the current branch to master
+# git push -f origin master  # Force push master branch to github
+# git gc --aggressive --prune=all     # remove the old files
