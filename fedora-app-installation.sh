21edf798313d467afde586596d6f2aa44d0868f0
dehvCurtis <dehvonc@gmail.com>
2022-01-31 15:39:22 -0700

    Update fedora-app-installation.sh

--- fedora-app-installation.sh
+++ fedora-app-installation.sh
@@ -25,6 +25,11 @@ sudo dnf install zsh
 # ohmyzsh
 wget sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Set ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
+source ~/.zshrc
+
 # ohmyzsh fonts
 cd /usr/share/fonts
 sudo mkdir meslo
