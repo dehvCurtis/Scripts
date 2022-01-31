cbe12f37f2b2dbaff7004c4db147173fb55ab9b9
dehvCurtis <dehvonc@gmail.com>
2022-01-31 15:16:57 -0700

    Update fedora-app-installation.sh

--- fedora-app-installation.sh
+++ fedora-app-installation.sh
@@ -1,8 +1,20 @@
+# Tools
+sudo dnf install tree vim git wget curl
+
+# Docker
+sudo dnf install docker-ce docker-ce-cli containerd.io
+
 # PyCharm
-sudo dnf upgrade --refresh -y~
+sudo dnf upgrade --refresh -y
 sudo dnf upgrade --refresh -y
 sudo whoami
 sudo dnf install dnf-plugins-core -y
 sudo dnf install python3 python3-devel python3-distutils-extra -y
 sudo nano /etc/yum.repos.d/phracek-PyCharm-fedora-35.repo
 sudo dnf install pycharm-community
+
+# zsh
+sudo dnf install zsh
+
+# ohmyzsh
+wget sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
