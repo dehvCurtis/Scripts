bc47a4c99e12a2e6b122b3ade5b7a44ecd828665
dehvCurtis <dehvonc@gmail.com>
2022-01-31 15:34:15 -0700

    Update fedora-app-installation.sh

--- fedora-app-installation.sh
+++ fedora-app-installation.sh
@@ -1,6 +1,12 @@
 # Tools
 sudo dnf install tree vim git wget curl
 
+# Brave Browser
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
 # Docker
 sudo dnf install docker-ce docker-ce-cli containerd.io
 
@@ -18,3 +24,12 @@ sudo dnf install zsh
 
 # ohmyzsh
 wget sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# ohmyzsh fonts
+cd /usr/share/fonts
+sudo mkdir meslo
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+sudo fc-cache -v
