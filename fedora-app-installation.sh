8e954cc60e5217c36442c6993fe4f5bcc873142f
dehvCurtis <dehvonc@gmail.com>
2022-01-31 15:09:53 -0700

    Create fedora-app-installation.sh

---
+++ fedora-app-installation.sh
@@ -0,0 +1,8 @@
+# PyCharm
+sudo dnf upgrade --refresh -y~
+sudo dnf upgrade --refresh -y
+sudo whoami
+sudo dnf install dnf-plugins-core -y
+sudo dnf install python3 python3-devel python3-distutils-extra -y
+sudo nano /etc/yum.repos.d/phracek-PyCharm-fedora-35.repo
+sudo dnf install pycharm-community
