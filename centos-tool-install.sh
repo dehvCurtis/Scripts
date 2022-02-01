2d84c20a4e35d49cee6d262ebaa9fa21923c878a
dehvCurtis <dehvonc@gmail.com>
2022-02-01 11:44:48 -0700

    Update centos-tool-install.sh

--- centos-tool-install.sh
+++ centos-tool-install.sh
@@ -1,10 +1,10 @@
 # OS
 sudo yum update -y
+sudo yum install epel-release -y
 sudo yum install tree vim git wget curl gcc docker -y
 reboot
 
 # Snap
-sudo yum install epel-release
 sudo yum install snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
@@ -21,3 +21,11 @@ sudo snap install typora
 
 # Terminator Terminal
 yum install -y terminator
+
+# Docker
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install -y yum-utils
+
+ sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
