092405aea45289cc95054946aa9934fdc0838a65
dehvCurtis <dehvonc@gmail.com>
2022-02-01 10:46:45 -0700

    Update centos-tool-install.sh

--- centos-tool-install.sh
+++ centos-tool-install.sh
@@ -8,8 +8,13 @@ sudo yum install snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
+# Snap Apps
 sudo snap install spotify
 sudo snap install 1password
 sudo snap install gimp
 sudo snap install typora
 sudo snap install brave
+sudo snap install slack --classic
+
+# Terminator Terminal
+yum install -y terminator
